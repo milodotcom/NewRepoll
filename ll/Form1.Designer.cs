1487e697d1dc2ca4ec06e6117f7c62177c4cb583
milo <anthanielaniel05@gmail.com>
2025-06-20 00:34:32 +0800

    hehe

---
+++ ll/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.practiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.delegateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lambdaExpressionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.builtinDelegatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.practiceToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // practiceToolStripMenuItem
+            // 
+            this.practiceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.delegateToolStripMenuItem,
+            this.lambdaExpressionsToolStripMenuItem,
+            this.builtinDelegatesToolStripMenuItem});
+            this.practiceToolStripMenuItem.Name = "practiceToolStripMenuItem";
+            this.practiceToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
+            this.practiceToolStripMenuItem.Text = "Practice";
+            this.practiceToolStripMenuItem.Click += new System.EventHandler(this.practiceToolStripMenuItem_Click);
+            // 
+            // delegateToolStripMenuItem
+            // 
+            this.delegateToolStripMenuItem.Name = "delegateToolStripMenuItem";
+            this.delegateToolStripMenuItem.Size = new System.Drawing.Size(226, 26);
+            this.delegateToolStripMenuItem.Text = "Delegate";
+            this.delegateToolStripMenuItem.Click += new System.EventHandler(this.delegateToolStripMenuItem_Click);
+            // 
+            // lambdaExpressionsToolStripMenuItem
+            // 
+            this.lambdaExpressionsToolStripMenuItem.Name = "lambdaExpressionsToolStripMenuItem";
+            this.lambdaExpressionsToolStripMenuItem.Size = new System.Drawing.Size(226, 26);
+            this.lambdaExpressionsToolStripMenuItem.Text = "Lambda Expressions";
+            // 
+            // builtinDelegatesToolStripMenuItem
+            // 
+            this.builtinDelegatesToolStripMenuItem.Name = "builtinDelegatesToolStripMenuItem";
+            this.builtinDelegatesToolStripMenuItem.Size = new System.Drawing.Size(226, 26);
+            this.builtinDelegatesToolStripMenuItem.Text = "Built-in Delegates";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ll.Properties.Resources.IMG_1712;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem practiceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem delegateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lambdaExpressionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem builtinDelegatesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ ll/Form2.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ll
+{
+    public partial class Form2 : Form
+    {
+
+        string appDate;
+        public delegate void DelegateOneString(string info);
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // Step 1.A.2.c
+            buttonNew.Enabled = false;
+            labelDetails.Enabled = false;
+        }
+
+        public void PrintName(string info)
+        {
+            labelDetails.Text = info;
+        }
+
+        private void buttonSet_Click(object sender, EventArgs e)
+        {
+            string owner = textBoxOwner.Text;
+            string cat = textBoxCat.Text;
+            string breed = comboBoxBreed.Text;
+
+            string info = $"Owner: {owner}\nCat: {cat}\nBreed: {breed}\nDate: {appDate}";
+
+            DelegateOneString Print = new DelegateOneString(PrintName);
+            Print(info);
+
+            labelDetails.Enabled = true;
+            buttonNew.Enabled = true;
+            buttonAppointment.Enabled = false;
+        }
+
+
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            buttonAppointment.Enabled = true;
+            buttonNew.Enabled = false;
+            labelDetails.Enabled = false;
+
+            textBoxOwner.Clear();
+            textBoxCat.Clear();
+            labelDetails.Text = "Appointment Details";
+        }
+
+        private void buttonAppointment_Click(object sender, EventArgs e)
+        {
+
+            DelegateOneString Print = PrintName; // Step 1.B.3.a
+            Print("Owner : " + textBoxOwner.Text); // Step 1.B.3.b
+            Print("Cat : " + textBoxCat.Text); // Step 1.B.3.b
+            Print("Breed : " + comboBoxBreed.SelectedItem); // Step 1.B.3.b
+            Print("Appointment Date : " + appDate); // Step 1.B.3.b
+            labelDetails.Enabled = true; // Step 1.B.3.c
+            buttonNew.Enabled = true; // Step 1.B.3.c
+            buttonAppointment.Enabled = false; // Step 1.B.3.d
+        }
+    }
+}
+
